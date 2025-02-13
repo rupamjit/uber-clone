7159287fbc38eb3902e8d47b1227a54e013f76a8
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-02-13 23:17:01 +0530

    add connect db

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ import dotenv from "dotenv"
 dotenv.config()
 import express from "express"
 import cors from "cors"
+import connectDB from "./db/db.js"
 const app = express()
 
+connectDB()
 app.get('/',(req,res)=>{
     res.send('Hello World')
 })
