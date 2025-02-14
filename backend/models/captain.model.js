d584421a1bc93198dfa25aadf7e6916846c885b8
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-02-14 15:42:57 +0530

    create captain model

---
+++ backend/models/captain.model.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const captainSchema = new mongoose.Schema({
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: [3, "Firstlength Must be atleast 3 charcaters "],
+    },
+    lastName: {
+      type: String,
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    minLength: [6, "Password must be atleast 6 chracters"],
+    select: false,
+  },
+  socketId: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive"],
+    default: "inactive",
+  },
+  vehicle: {
+    color: {
+      type: String,
+      required: true,
+      minlength: [3, "Color must be at least 3 characters long"],
+    },
+    plate: {
+      type: String,
+      type: String,
+      required: true,
+      minlength: [3, "Plate must be at least 3 characters long"],
+    },
+    capacity: {
+      type: Number,
+      required: true,
+      min: [1, "Capacity must be at least 1"],
+    },
+    vehicleType: {
+      type: String,
+      required: true,
+      enum: ["car", "motorcycle", "auto"],
+    },
+  },
+  location: {
+    ltd: {
+      type: Number,
+    },
+    lng: {
+      type: Number,
+    },
+  },
+});
+
+const Captain = mongoose.model("Captain", captainSchema);
+export default Captain;
