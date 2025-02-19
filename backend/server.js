74696314f7b549dca40676a96a4b78d510dc0e4c
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-02-19 16:04:41 +0530

    set up socket.io in the backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-import app from "./app.js"
+import http from "http";  
+import app from "./app.js";
+import { initializeSocket } from "./socket.js";
 
+const port = process.env.PORT || 3000;
 
+const server = http.createServer(app);  
+initializeSocket(server);
 
-const port =process.env.PORT || 3000
-app.listen(port, ()=>{
-    console.log(`Server listening on ${port}`);
-})
+server.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
